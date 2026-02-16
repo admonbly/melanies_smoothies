6f13ee2d691fd85cfb6c3364830a4872e0d1d827
admonbly <economistededemain@gmail.com>
2026-02-16 17:45:41 +0100

    Modify smoothie options to include SEARCH_ON column
    Updated the data retrieval to include 'SEARCH_ON' column and added a dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,20 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-
 Name_on_order = st.text_input('Name on smothie:')
 st.write("The name on your smothie will be", Name_on_order)
-
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME')),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
 
